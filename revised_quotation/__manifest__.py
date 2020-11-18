34518dd65ba8b92d1c8220a9d487542414ca2fa0
k3zone <71325494+k3zone@users.noreply.github.com>
2020-11-18 17:44:05 +0400

    Add files via upload

---
+++ revised_quotation/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Revised Quotation',
+    'author': 'BroadPeak',
+    'category': 'sales',
+    'license': 'AGPL-3',
+    'website': 'www.broadpeak.co',
+    'version': '14.0.1.0.0',
+    'summary': '''
+                Quotation Revised History
+            ''',
+    'description': '''
+                    This module can manage Quotation Revised History in sale order.
+                ''',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
